6110459ffa2dc3d6506a8395dd25f6187e71c2ab
Angel Campos <th3lowfiguy@gmail.com>
2022-05-23 11:16:57 -0500

    week 4 day 1

--- python/flask_mysql/belt_review/recipies_camposa/flask_app/models/recipe.py
+++ python/flask_mysql/belt_review/recipies_camposa/flask_app/models/recipe.py
@@ -1,4 +1,3 @@
-from click import edit
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app.models import user
--- python/js/algo3.js
+++ python/js/algo3.js
@@ -30,27 +30,85 @@ const expected5 = null;
  *    dupes or null if there is none.
  */
 
-function firstNonRepeated(nums){
-    if (nums.length < 1){
+function firstNonRepeated(nums) {
+    if (nums.length < 1) {
         return null;
     }
     var expected = {};
-    for (var num of nums){
-        if (!expected[num]){
+    for (var num of nums) {
+        if (!expected[num]) {
             expected[num] = 1;
         } else {
             expected[num]++;
         }
-    } 
-        console.log(expected)
-        for(var num of nums){
-            if(expected[num] == 1){
-                return num;
-            }
+    }
+    console.log(expected)
+    for (var num of nums) {
+        if (expected[num] == 1) {
+            return num;
         }
+    }
 }
 console.log(firstNonRepeated(nums1))
 console.log(firstNonRepeated(nums2))
 console.log(firstNonRepeated(nums3))
 console.log(firstNonRepeated(nums4))
 console.log(firstNonRepeated(nums5))
+
+function bookIndex(pages) {
+    var strArr = [];
+    var str = "";
+    var seqStart;
+
+    for (var i = 0; i < pages.length; i++) {
+        seqStart = pages[i]
+        var seqEnd = undefined;
+
+        for (var j = i + 1; j < pages.length; j++) {
+            if (pages[j] == pages[j - 1] + 1) {
+                seqEnd = pages[j];
+                if (j == pages.length - 1) {
+                    i = j;
+                    break;
+                }
+            } else if (seqEnd == undefined) {
+                i = j - 1;
+                break;
+            } else {
+                i = j - 1;
+                break;
+            }
+        }
+        if (seqEnd != undefined) {
+            strArr.push('' + seqStart + '-' + seqEnd)
+        } else {
+            strArr.push('' + seqStart)
+        }
+    }
+    for (var k = 0; k < strArr.length; k++) {
+        if (k == strArr.length - 1) {
+            str += strArr[k]
+        } else {
+            str += strArr[k] + ", ";
+        }
+    }
+
+    return str;
+
+}
+
+function recursiveSigma(input) {
+    if (input <= 0) {
+        return 0;
+    } else {
+        return input + recursiveFactorial(input - 1);
+    }
+}
+
+function recursiveFactorial(input) {
+    if (input <= 1) {
+        return 1;
+    } else {
+        return input * recursiveFactorial(input - 1);
+    }
+}
