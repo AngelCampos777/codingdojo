b33c1f8c641c127da881e4ce34c55dc8e938eebe
Angel Campos <th3lowfiguy@gmail.com>
2022-09-08 22:14:20 -0500

    update

--- python/projectNIftyStore/server.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app
-from flask_app.controllers import #insert controllername(s)
-#may have many controllers name(s)_controller, name2(s)_controller
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
